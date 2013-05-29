ddc4a9d55e8ed6ee09cf0783f182bb75ed911620
sycy82@gmail.com <sycy82@gmail.com@cb7795c5-7d62-9e95-77a3-6a6313892f73>
2013-05-29 07:17:36 +0000

    增加网站监控的测试功能

--- tags/paramecium/src/demo/test/com/demo/test/HttpClientTest.java
+++ tags/paramecium/src/demo/test/com/demo/test/HttpClientTest.java
@@ -9,6 +9,64 @@ package com.demo.test;
  */
 public class HttpClientTest {
 	
+	/*volatile static int countor = 0;
+	volatile static int threadNo = 0;
+
+	synchronized static int printCount() {
+		return countor++;
+	}
+	
+	synchronized static int printThreadNo() {
+		return threadNo++;
+	}
+
+	public static void main(String[] args) throws Exception {
+		for (int i = 0; i < 100; i++) {
+			new Thread(new HttpClientTest().new ThreadHttp(printThreadNo())).start();
+		}
+	}
+
+	class ThreadHttp implements Runnable {
+		volatile int threadNo = 0;
+		
+		public ThreadHttp(int threadNo){
+			this.threadNo = threadNo;
+		}
+		public void run() {
+			while (true) {
+				if (countor >= 1000) {
+					try {
+						System.out.println(this.threadNo + "该线程已经挂起!");
+						//Thread.sleep(1000 * 10 * 60);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+					return;
+					//initI();
+					//continue;
+				}
+				HttpClient httpclient = new DefaultHttpClient();
+				httpclient.getParams().setIntParameter("http.socket.timeout",5000);
+				HttpGet httpget = new HttpGet("http://www.baidu.com/");
+				HttpResponse response = null;
+				try {
+					response = httpclient.execute(httpget);
+					int code = response.getStatusLine().getStatusCode();
+					if(code<400){
+						System.out.println(this.threadNo + "线程累计扫描：" + printCount() + " 状态码:" + response.getStatusLine().getStatusCode());
+					}else{
+						System.err.println("网页有错误，错误码为："+code);
+					}
+				} catch (ClientProtocolException e) {
+				} catch (java.net.SocketTimeoutException e) {
+					System.err.println("超时了。。。");
+				} catch (IOException e) {
+				}
+			}
+		}
+
+	}*/
+	
 	public static void main(String[] args) throws Exception {
 		/*List<NameValuePair> formparams = new ArrayList<NameValuePair>();
 		formparams.add(new BasicNameValuePair("username", "admin"));
